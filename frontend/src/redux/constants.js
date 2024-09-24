8f5af572d37b6397a74eb9e3660d1d2bb7f00b2e
vishnupancharatnala <131269267+vishnupancharatnala@users.noreply.github.com>
2024-09-24 09:37:12 +0530

    Update constants.js

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://my-movies-59aq.onrender.com";
+export const BASE_URL = "https://my-movies-98ek.onrender.com";
 export const USERS_URL = "/api/v1/users";
 export const GENRE_URL = "/api/v1/genre";
 export const MOVIE_URL = "/api/v1/movies";
